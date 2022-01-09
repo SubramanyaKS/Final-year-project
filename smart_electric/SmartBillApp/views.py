726e0f35be550c5b08ce095e16e32cad1bf5960b
Subramanya K S <subramanyaks2205@gmail.com>
2022-01-09 09:11:59 +0530

    updated login and main

--- smart_electric/SmartBillApp/views.py
+++ smart_electric/SmartBillApp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def home(request):
     return render(request,"index.html")
 
+def login(request):
+    return render(request,"login.html")
+
--- smart_electric/smart_electric/urls.py
+++ smart_electric/smart_electric/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from SmartBillApp.views import login
 from SmartBillApp.views import home
 from django.contrib import admin
 
@@ -22,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',home),
     path('',home),
+    path('login/',login),
 ]
