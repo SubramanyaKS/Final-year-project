9450e870dca0deb8fda68ff70fb9e119f1ac0534
Subramanya K S <70049779+SubramanyaKS@users.noreply.github.com>
2022-05-19 12:42:11 +0530

    Delete smart_electric directory

--- smart_electric/SmartBillApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmartbillappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SmartBillApp'
--- smart_electric/SmartBillApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.db.models import Model
-# Create your models here.
-
-class ElectricModel(Model):
-	user_field = models.TextField()
-
-# Create your models here.
